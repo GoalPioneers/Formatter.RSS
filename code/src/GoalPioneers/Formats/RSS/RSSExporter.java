001fce8c0ad35f557f212872a0a3ba4c36744f24
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-04 14:06:28 +0200

    staging

--- code/src/GoalPioneers/Formats/RSS/RSSExporter.java
+++ code/src/GoalPioneers/Formats/RSS/RSSExporter.java
@@ -1,6 +1,20 @@
 package GoalPioneers.Formats.RSS;
 
+import GoalPioneers.Formats.Templates.GraphFiles.GraphExporterFacade;
+
+
+/**
+ * 
+ */
 public class RSSExporter 
+        implements GraphExporterFacade
 {
+    /**
+     * 
+     */
+    public RSSExporter()
+    {
+        
+    }
     
 }
--- code/src/GoalPioneers/Formats/RSS/RSSImporter.java
+++ code/src/GoalPioneers/Formats/RSS/RSSImporter.java
@@ -1,10 +1,13 @@
 package GoalPioneers.Formats.RSS;
 
 
+import GoalPioneers.Formats.Templates.GraphFiles.GraphImporterFacade;
+
 /**
  * 
  */
 public class RSSImporter 
+        implements GraphImporterFacade
 {
     // Constructor
     public RSSImporter()
